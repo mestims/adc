9ad9da0f479ee416771f3293feba62bd1a611c6e
Matheus <msambiase@uolinc.com>
2019-10-10 16:54:51 -0300

    first commit

---
+++ src/main/kotlin/br/com/adc/AdcApplication.kt
@@ -0,0 +1,11 @@
+package br.com.adc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdcApplication
+
+fun main() {
+    runApplication<AdcApplication>()
+}
